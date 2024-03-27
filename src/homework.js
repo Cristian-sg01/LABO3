fd2e3c45c6aae0ecf81a545b3b8c0828541eca42
01_Cristian_01 <sunaguagira@gmail.com>
2024-03-27 18:59:16 -0400

    corregido

--- src/homework.js
+++ src/homework.js
@@ -5,10 +5,12 @@
   /* First excercise */
   
   let numbers = [1, 2, 3, 4, 5];
-  
-  /* Multiplique los numeros del arreglo por 5 */
 
-  /*Resultado esperado: [5, 10, 15, 20, 25] */
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] *= 5;
+}
+console.log(numbers); 
+  
 
   /* First excercise */
 
@@ -16,9 +18,9 @@
 
   let names = ["Jhosep", "Daniel", "Rodrigo", "Veronica"];
 
-  /* Ordernar los nombres alfabeticamente */
+names.sort();
 
-  /* Resultado esperado: ["Daniel", "Jhosep", "Rodrigo", "Veronica"] */
+console.log(names); 
 
   /* Second excercise */
 
@@ -26,8 +28,12 @@
 
   const data = ['a', 'b', 'z', 'r', 'a', 'c', 'r', 'a'];
 
-  /* Dado un arreglo, crear otro sin letras repetidas */
-  /* Resultado esperado: ['a', 'b', 'z', 'r', 'c'] */
+const uniqueSet = new Set(data);
+
+const uniqueData = Array.from(uniqueSet);
+
+console.log(uniqueData); 
+
 
   /* Thrid excercise */
 
@@ -35,20 +41,18 @@
 
   const largeWord = "abcdabcdABCD";
 
-  /* Dado un string mostrar cuantas veces se repite cada letra*/
-  /* Resultado: 
-    {
-      a: 2,
-      b: 2,
-      c: 2,
-      d: 2,
-      A: 1,
-      B: 1,
-      C: 1,
-      D: 1
+  const letterCount = {};
+  
+  const lowerCaseWord = largeWord.toLowerCase();
+  
+  for (const letter of lowerCaseWord) {
+    if (!letterCount[letter]) {
+      letterCount[letter] = 1;
+    } else {
+      letterCount[letter]++;
     }
-  */
-
+  }
+    console.log(letterCount); 
   /* Fourth excercise */
 
 })();
